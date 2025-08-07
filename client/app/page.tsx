0c4234c038c7ffaa31f7c676fa9f230e442f1a5e
yomjsty <oweychanges@gmail.com>
2025-08-07 23:51:30 +0700

    use client

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,27 +1,45 @@
-import Link from 'next/link';
-import { SignoutButton } from '@/components/SignoutButton';
-import getServerSession from '@/lib/server-session';
+'use client';
 
-export default async function Home() {
-	const session = await getServerSession();
+import { LayoutDashboardIcon } from 'lucide-react';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
+import { cn } from '@/lib/utils';
+import { SignoutButton } from '../components/SignoutButton';
+
+export default function Home() {
+	const { data: session, isPending } = authClient.useSession();
 
 	return (
-		<div className="flex h-screen flex-col items-center justify-center space-y-4">
-			{session ? (
-				<>
-					You are signed in as {session.user.name}.{' '}
-					<SignoutButton description="Logout" />
-				</>
-			) : (
-				<>
-					<Link href="/login">Login</Link>
-					<Link href="/register">Register</Link>
-				</>
+		<div className="flex min-h-screen flex-col items-center justify-center gap-4">
+			<h1 className="font-bold text-2xl">Home</h1>
+			<p>
+				Playing with <span className="font-medium">Better-Auth</span> on{' '}
+				<span className="font-medium">
+					Next.js and Hono as a backend
+				</span>
+			</p>
+			{isPending && null}
+			{!isPending && session && (
+				<div className="flex gap-2">
+					<Link
+						className={cn(buttonVariants({ variant: 'outline' }))}
+						href="/dashboard"
+					>
+						<LayoutDashboardIcon className="size-4" />
+						Dashboard
+					</Link>
+					<SignoutButton />
+				</div>
 			)}
-			<div className="flex flex-col gap-2">
-				<Link href="/protected">Fetch data from protected route</Link>
-				<Link href="/normal">Fetch data from non-protected route</Link>
-			</div>
+			{!(isPending || session) && (
+				<Link
+					className={cn(buttonVariants({ variant: 'outline' }))}
+					href="/login"
+				>
+					Login
+				</Link>
+			)}
 		</div>
 	);
 }
