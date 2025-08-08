22cd40b40b8273fac11371a466908815f00314e2
yomjsty <oweychanges@gmail.com>
2025-08-08 08:12:09 +0700

    first commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,28 @@
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { auth } from './lib/auth.js';
 import { env } from './lib/env.js';
 import route from './routes/route.js';
-import { serve } from "@hono/node-server";
 
 const app = new Hono();
 
 app.use(logger());
+
 app.use(
 	'*',
 	cors({
 		origin: env.CORS_ORIGIN,
-		allowMethods: ['GET', 'POST', 'OPTIONS'],
 		allowHeaders: ['Content-Type', 'Authorization'],
+		allowMethods: ['POST', 'GET', 'OPTIONS'],
+		exposeHeaders: ['Content-Length'],
+		maxAge: 600,
 		credentials: true,
 	})
 );
 
-app.use(
-	"*", // or replace with "*" to enable cors for all routes
-	cors({
-		origin: "http://localhost:3001", // replace with your origin
-		allowHeaders: ["Content-Type", "Authorization"],
-		allowMethods: ["POST", "GET", "OPTIONS"],
-		exposeHeaders: ["Content-Length"],
-		maxAge: 600,
-		credentials: true,
-	}),
-);
-
-app.on(["POST", "GET"], "/api/auth/*", (c) => {
+app.on(['POST', 'GET'], '/api/auth/*', (c) => {
 	return auth.handler(c.req.raw);
 });
 
